9a0bb4e50c3386f1e2fdeef81f99a684b8053d41
messerc <messer55@gmail.com>
2018-02-05 20:48:31 -0800

    added authentication - subscription to data next

--- src/components/LinkList.js
+++ src/components/LinkList.js
@@ -1,22 +1,25 @@
-import React, { Component } from 'react'
-import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
-import Link from './Link'
-
+import React, { Component } from "react";
+import { graphql } from "react-apollo";
+import gql from "graphql-tag";
+import Link from "./Link";
 
 class LinkList extends Component {
   render() {
     const { loading, error, feed } = this.props.feedQuery;
     if (loading) {
-      return <div>Loading</div>
+      return <div>Loading</div>;
     }
     if (error) {
-      return <div>Error</div>
+      return <div>Error</div>;
     }
     const linksToRender = feed.links;
     return (
-      <div>{linksToRender.map(link => <Link key={link.id} link={link} />)}</div>
-    )
+      <div>
+        {linksToRender.map((link, i) => (
+          <Link key={link.id} index={i} link={link} />
+        ))}
+      </div>
+    );
   }
 }
 
@@ -30,10 +33,20 @@ const FEED_QUERY = gql`
         createdAt
         url
         description
+        postedBy {
+          id
+          name
+        }
+        votes {
+          id
+          user {
+            id
+          }
+        }
       }
     }
   }
-`
+`;
 
 // 3
-export default graphql(FEED_QUERY, { name: 'feedQuery' })(LinkList)
+export default graphql(FEED_QUERY, { name: "feedQuery" })(LinkList);
